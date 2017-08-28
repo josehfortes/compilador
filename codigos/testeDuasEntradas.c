eb7c4236f34d1a891676482dcd7ab723143354df
Jose Henrique <vasco0007@gmail.com>
2017-08-28 11:02:30 -0300

    Adicionado código de duas entradas

---
+++ codigos/testeDuasEntradas.c
@@ -0,0 +1,20 @@
+int input(void)
+{
+}
+
+void output(int out)
+{
+}
+
+void main(void)
+{
+    int number;
+    int n;
+    int res;
+    number = input();
+    output(number);
+    n = input();
+    output(n);
+    res = number * n;
+    output(res);
+}
