93726645125d97c2a9d7ebbcc00af0ae51283171
Jose Henrique <vasco0007@gmail.com>
2017-12-11 11:58:16 -0200

    inseirdo so

--- codigos/so.c
+++ codigos/so.c
@@ -1,5 +1,5 @@
-int execucao[12];
-int pc[12];
+int execucao[3];
+int pc[3];
 int maxProgram;
 int programAtual;
 
@@ -12,7 +12,11 @@ void output(int out)
 {
 }
 
-void salvaPrograma(void){
+void alteraContexto(int i){
+  /* carrega os registradores e altera o contexto global */
+  pc[programAtual] = 123; /* alterar para armazenar o PC do reg30 aqui */
+  programAtual = i;
+  pc[programAtual] = 456; /* alterar pra dar jump no i = pc[programAtual] */
   programAtual = 666;
   programAtual = 666;
   programAtual = 666;
@@ -24,25 +28,40 @@ void salvaPrograma(void){
   programAtual = 666;
   programAtual = 666;
   programAtual = 666;
+
+}
+
+void buscaPrograma(void){
+  int i;
+  int found;
+  found = 0;
+  i = programAtual;
+  i = i + 1;
+  if(i > maxProgram)
+    i = 1;
+  while(found == 0){
+    /* procura o proximo programa*/
+    if(execucao[i] == 1){
+      alteraContexto(i);
+    }
+    i = i + 1;
+    if(i > maxProgram)
+      i = 1;
+  }
 }
 
 
 void main(void)
 {
-  maxProgram = 4;
+  maxProgram = 2;
   programAtual = 1;
   execucao[0] = 1;
   execucao[1] = 1;
   execucao[2] = 1;
-  execucao[3] = 0;
-  execucao[4] = 0;
   pc[0] = 0;
   pc[1] = 0;
   pc[2] = 0;
-  pc[3] = 0;
-  pc[4] = 0;
-
-
-  /*a partir daqui, salva o programa*/
-  salvaPrograma();
+    /*a partir daqui, salva o programa*/
+  pc[programAtual] = 33;
+  buscaPrograma();
 }
